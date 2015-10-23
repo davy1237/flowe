fe6056d7a270f7f75172b4a7d6350b3ed7fb88dc
Malachy Donovan <malachyd@38.122.127.226.wireless-guest.net.tufts.edu>
2015-10-23 19:16:39 -0400

    Initial Commit

---
+++ Temp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Temp
+//
+//  Created by Malachy Donovan on 10/23/15.
+//  Copyright (c) 2015 Malachy Donovan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Temp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Temp
+//
+//  Created by Malachy Donovan on 10/23/15.
+//  Copyright (c) 2015 Malachy Donovan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
